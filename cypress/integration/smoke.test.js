e86eff69ae4e7c8c5a36fec9e3f806d201a12777
Richard Cyrus <richard.cyrus@rcyrus.com>
2021-08-02 09:48:58 -0400

    Add accessibility smoke test

--- cypress/integration/smoke.test.js
+++ cypress/integration/smoke.test.js
@@ -1,3 +1,16 @@
+function testA11y() {
+  cy.injectAxe()
+  ;['iphone-6', 'ipad-2', 'macbook-15'].forEach((size) => {
+    if (Cypress._.isArray(size)) {
+      cy.viewport(size[0], size[1])
+    } else {
+      cy.viewport(size)
+    }
+    cy.get('h1').contains('Where in the world?')
+    cy.checkA11y()
+  })
+}
+
 const visit = (darkAppearance) =>
   cy.visit('/', {
     onBeforeLoad(win) {
@@ -9,13 +22,23 @@ const visit = (darkAppearance) =>
     },
   })
 
-describe('The HomePage', () => {
-  beforeEach(() => {
-    visit(false)
+describe('a11y', () => {
+  describe('light theme', () => {
+    it('should be accessible', () => {
+      visit(false)
+      testA11y()
+    })
   })
 
-  it('Theme toggle toggles the theme to dark mode', () => {
-    cy.contains('Dark Mode').click()
-    cy.get('body').should('have.class', 'dark')
+  describe('dark theme', () => {
+    it('should be accessible', () => {
+      visit(true)
+      testA11y()
+    })
   })
+
+  // it('Theme toggle toggles the theme to dark mode', () => {
+  //   cy.contains('Dark Mode').click()
+  //   cy.get('body').should('have.class', 'dark')
+  // })
 })
